53f62db6c87a6a4093db192018049c7bd0ed1833
digitalpersonal <112121450+digitalpersonal@users.noreply.github.com>
2026-03-20 03:19:04 -0300

    Delete services directory

--- services/geminiService.ts
+++
@@ -1,7 +0,0 @@
-
-// This service is currently disabled in favor of manual image uploads.
-// We export a dummy object to prevent import errors in other files.
-
-export const getGeminiClient = () => {
-  return null;
-};
